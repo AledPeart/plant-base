2c3bdd972f8319c76cdc21cfe16875430efffceb
AledPeart <aledpeart@gmail.com>
2021-10-14 09:54:24 +0000

    Connected Flask to MongoDB, updated requirements.txt

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+mongo = PyMongo(app)
+
+
 @app.route("/")
+@app.route("/get_sheets")
 
 
-def hello():
-    return "Hello World....again!"
+def get_sheets():
+    sheets = mongo.db.cheat_sheet.find()
+    return render_template("sheets.html", cheat_sheet = sheets)
 
 
 if __name__ == "__main__":
