51267048a9404d7f5ad4c628c1f6a4735ceb0a24
AledPeart <aledpeart@gmail.com>
2021-10-30 09:15:09 +0000

    Added style changes to the navbar, home page and sheets page changes to make site more responsive

--- static/js/script.js
+++ static/js/script.js
@@ -13,3 +13,12 @@ function myFunction() {
 $("#flash-messages").fadeTo(3000, 500).slideUp(500, function(){
   $("#flash-messages").slideUp(500);
 });
+
+//function to add an image-overlay box on the home page on larger devices https://stackoverflow.com/questions/29593944/css-media-query-adding-class-to-html/29594124
+
+var $homeBox = $('.home-box');
+
+$(window).resize(function() {
+  if (window.innerWidth >= 992) $homeBox.addClass('card-img-overlay');
+  else $homeBox.removeClass('card-img-overlay');
+});
